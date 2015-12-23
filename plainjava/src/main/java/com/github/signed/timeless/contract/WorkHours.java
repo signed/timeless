ae7c4298d78f6f52031f54d7cd208ef9d0409661
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-23 23:00:37 +0100

    Made WorkHours know about christmas and new years eve

--- plainjava/src/main/java/com/github/signed/timeless/contract/WorkHours.java
+++ plainjava/src/main/java/com/github/signed/timeless/contract/WorkHours.java
@@ -5,15 +5,34 @@ import org.joda.time.Duration;
 import org.joda.time.LocalDate;
 
 import com.github.signed.timeless.HoursRequired;
-import com.github.signed.timeless.WorkHoursPerDay;
+import com.github.signed.timeless.workhours.WorkHoursPerDay;
 
-public class WorkHours implements HoursRequired{
+public class WorkHours implements HoursRequired {
 
     @Override
     public WorkHoursPerDay hoursToWorkAt(LocalDate day) {
-        if( day.dayOfWeek().get() == DateTimeConstants.SATURDAY || day.dayOfWeek().get() == DateTimeConstants.SUNDAY){
+        if (day.dayOfWeek().get() == DateTimeConstants.SATURDAY || day.dayOfWeek().get() == DateTimeConstants.SUNDAY) {
             return WorkHoursPerDay.unreducedWorkHours(Duration.ZERO);
         }
-        return WorkHoursPerDay.unreducedWorkHours(Duration.standardHours(8));
+        if (isChristmas(day) || isNewYearsEve(day)) {
+            return WorkHoursPerDay.unreducedWorkHours(halfAWorkday());
+        }
+        return WorkHoursPerDay.unreducedWorkHours(workday());
+    }
+
+    private boolean isChristmas(LocalDate day) {
+        return day.monthOfYear().get() == DateTimeConstants.DECEMBER && day.dayOfMonth().get() == 24;
+    }
+
+    private boolean isNewYearsEve(LocalDate day) {
+        return day.monthOfYear().get() == DateTimeConstants.DECEMBER && day.dayOfMonth().get() == 31;
+    }
+
+    private Duration halfAWorkday() {
+        return workday().dividedBy(2);
+    }
+
+    private Duration workday() {
+        return Duration.standardHours(8);
     }
 }
--- plainjava/src/test/java/com/github/signed/timeless/contract/WorkHoursTest.java
+++ plainjava/src/test/java/com/github/signed/timeless/contract/WorkHoursTest.java
@@ -3,13 +3,19 @@ package com.github.signed.timeless.contract;
 import static com.github.signed.timeless.storage.DateTimeMother.AnySaturday;
 import static com.github.signed.timeless.storage.DateTimeMother.AnySunday;
 import static com.github.signed.timeless.storage.DateTimeMother.AnyWeekDayBetweenMondayAndFriday;
+import static com.github.signed.timeless.storage.DateTimeMother.ChristmasOnAWorkDay;
+import static com.github.signed.timeless.storage.DateTimeMother.NewYearsEveOnAWeekend;
+import static com.github.signed.timeless.storage.DateTimeMother.NewYearsEveOnAWorkDay;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.joda.time.Duration.ZERO;
 import static org.joda.time.Duration.standardHours;
 
 import org.joda.time.Duration;
 import org.joda.time.LocalDate;
 import org.junit.Test;
+
+import com.github.signed.timeless.storage.DateTimeMother;
 
 public class WorkHoursTest {
 
@@ -19,22 +25,50 @@ public class WorkHoursTest {
     @Test
     public void saturdayIsWorkFree() throws Exception {
         day = AnySaturday();
-        assertThat(hoursToWorkAtGivenDay(day), is(Duration.ZERO));
+        assertThat(hoursToWorkAt(day), is(ZERO));
     }
 
     @Test
     public void sundayIsWorkFree() throws Exception {
         day = AnySunday();
-        assertThat(hoursToWorkAtGivenDay(day), is(Duration.ZERO));
+        assertThat(hoursToWorkAt(day), is(ZERO));
     }
 
     @Test
     public void anyOtherDayOfTheWeekItIsEightHoursOfWorkTime() throws Exception {
         day = AnyWeekDayBetweenMondayAndFriday();
-        assertThat(hoursToWorkAtGivenDay(day), is(standardHours(8)));
+        assertThat(hoursToWorkAt(day), is(normalWorkDay()));
     }
 
-    private Duration hoursToWorkAtGivenDay(LocalDate day) {
+    @Test
+    public void christmasIsOnlyHalfADayOfWork() throws Exception {
+        assertThat(hoursToWorkAt(ChristmasOnAWorkDay()), is(halfANormalWorkDay()));
+    }
+
+    @Test
+    public void christmasOnTheWeekendIsStillFree() throws Exception {
+        assertThat(hoursToWorkAt(DateTimeMother.ChristmasOnAWeekend()), is(ZERO));
+    }
+
+    @Test
+    public void newYearsEveIsOnlyHalfADayOfWork() throws Exception {
+        assertThat(hoursToWorkAt(NewYearsEveOnAWorkDay()), is(halfANormalWorkDay()));
+    }
+
+    @Test
+    public void newYearsEveOnTheWeekEndIsStillFree() throws Exception {
+        assertThat(hoursToWorkAt(NewYearsEveOnAWeekend()), is(ZERO));
+    }
+
+    private Duration halfANormalWorkDay() {
+        return normalWorkDay().dividedBy(2);
+    }
+
+    private Duration normalWorkDay() {
+        return standardHours(8);
+    }
+
+    private Duration hoursToWorkAt(LocalDate day) {
         return workHours.hoursToWorkAt(day).duration();
     }
 }
