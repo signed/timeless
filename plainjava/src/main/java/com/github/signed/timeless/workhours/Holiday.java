ee23d59d7213bb142f2319a243080852d2e8811e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-25 23:20:27 +0100

    Calculate holidays for the entire year

---
+++ plainjava/src/main/java/com/github/signed/timeless/workhours/Holiday.java
@@ -0,0 +1,11 @@
+package com.github.signed.timeless.workhours;
+
+import org.joda.time.LocalDate;
+
+public class Holiday {
+    public final LocalDate date;
+
+    public Holiday(LocalDate date) {
+        this.date = date;
+    }
+}
---
+++ plainjava/src/main/java/com/github/signed/timeless/workhours/HolidayCalculator.java
@@ -0,0 +1,35 @@
+package com.github.signed.timeless.workhours;
+
+import static org.joda.time.DateTimeConstants.DECEMBER;
+import static org.joda.time.DateTimeConstants.JANUARY;
+import static org.joda.time.DateTimeConstants.MAY;
+import static org.joda.time.DateTimeConstants.NOVEMBER;
+import static org.joda.time.DateTimeConstants.OCTOBER;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.LocalDate;
+
+public class HolidayCalculator {
+
+    private final EasterSundayCalculator calculator = new EasterSundayCalculator();
+
+    public List<Holiday> holidaysFor(int year) {
+        List<Holiday> holidays = new ArrayList<Holiday>();
+        holidays.add(new Holiday(new LocalDate(year, JANUARY, 1)));
+        holidays.add(new Holiday(new LocalDate(year, JANUARY, 6)));
+        LocalDate easterSunday = calculator.easterSundayFor(2015);
+        holidays.add(new Holiday(easterSunday.minusDays(2)));
+        holidays.add(new Holiday(easterSunday.plusDays(1)));
+        holidays.add(new Holiday(new LocalDate(year, MAY, 1)));
+        holidays.add(new Holiday(easterSunday.plusDays(39)));
+        holidays.add(new Holiday(easterSunday.plusDays(50)));
+        holidays.add(new Holiday(easterSunday.plusDays(60)));
+        holidays.add(new Holiday(new LocalDate(year, OCTOBER, 3)));
+        holidays.add(new Holiday(new LocalDate(year, NOVEMBER, 1)));
+        holidays.add(new Holiday(new LocalDate(year, DECEMBER, 25)));
+        holidays.add(new Holiday(new LocalDate(year, DECEMBER, 26)));
+        return holidays;
+    }
+}
