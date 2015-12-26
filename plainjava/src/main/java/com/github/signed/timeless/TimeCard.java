193cec75f52aa1f76348ab5a3328da2a3a66fc63
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-26 01:15:21 +0100

    A time card has a from and an until

--- plainjava/src/main/java/com/github/signed/timeless/TimeCard.java
+++ plainjava/src/main/java/com/github/signed/timeless/TimeCard.java
@@ -2,14 +2,33 @@ package com.github.signed.timeless;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
+
+import org.joda.time.LocalDate;
 
 public class TimeCard implements Iterable<Punch>{
 
-    private final Collection<Punch> punches;
+    private final List<Punch> punches;
 
     public TimeCard(Collection<Punch> punches) {
         this.punches = new ArrayList<Punch>(punches);
+        Collections.sort(this.punches, new Comparator<Punch>() {
+            @Override
+            public int compare(Punch o1, Punch o2) {
+                return o1.dateTime().compareTo(o2.dateTime());
+            }
+        });
+    }
+
+    public LocalDate from() {
+        return punches.get(0).dateTime().toLocalDate();
+    }
+
+    public LocalDate until() {
+        return punches.get(punches.size()-1).dateTime().toLocalDate();
     }
 
     @Override
---
+++ plainjava/src/test/java/com/github/signed/timeless/TimeCard_Test.java
@@ -0,0 +1,35 @@
+package com.github.signed.timeless;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.signed.timeless.storage.DateTimeMother;
+import com.github.signed.timeless.storage.WorkLogBuilder;
+
+public class TimeCard_Test {
+
+    private final WorkLogBuilder workLogBuilder = new WorkLogBuilder();
+    private final LocalDate firstWorkDay = DateTimeMother.AnyWorkday();
+    private final LocalDate lastWorkDay = firstWorkDay.plusDays(3);
+
+    @Before
+    public void logWorkOverMultipleDaysInWrongOrder() throws Exception {
+        workLogBuilder.on(lastWorkDay).workedFrom(FromUntilMother.anyAmountOfTime());
+        workLogBuilder.on(firstWorkDay).workedFrom(FromUntilMother.anyAmountOfTime());
+    }
+
+    @Test
+    public void deriveFromDayFromFirstPunch() throws Exception {
+        assertThat(workLogBuilder.timeCard().from(), is(firstWorkDay));
+    }
+
+
+    @Test
+    public void deriveUntilDayFormLastPunch() throws Exception {
+        assertThat(workLogBuilder.timeCard().until(), is(lastWorkDay));
+    }
+}
