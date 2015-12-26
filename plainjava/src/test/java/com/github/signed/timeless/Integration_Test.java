2e03f73ccd742808492429f897411332eac56704
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-12-26 12:51:58 +0100

    Add new adjusters to integration test

--- plainjava/src/test/java/com/github/signed/timeless/Integration_Test.java
+++ plainjava/src/test/java/com/github/signed/timeless/Integration_Test.java
@@ -15,25 +15,30 @@ import com.github.signed.timeless.contract.WorkHours;
 import com.github.signed.timeless.storage.DateTimeBuilder;
 import com.github.signed.timeless.storage.DateTimeMother;
 import com.github.signed.timeless.storage.WorkLogBuilder;
+import com.github.signed.timeless.workhours.ConferenceDays;
 import com.github.signed.timeless.workhours.Holidays;
 import com.github.signed.timeless.workhours.PersonalTimeOff;
+import com.github.signed.timeless.workhours.SickLeave;
 import com.github.signed.timeless.workhours.WorkHoursPerDayAdjuster;
 import com.github.signed.timeless.workhours.WorkHoursPerDayCompendium;
 
 public class Integration_Test {
+    private final WorkHours workHours = new WorkHours();
+    private final PersonalTimeOff personalTimeOff = new PersonalTimeOff();
+    private final Holidays holidays = new Holidays();
+    private final ConferenceDays conferenceDays = new ConferenceDays();
+    private final SickLeave sickLeave = new SickLeave();
 
     @Test
     public void smoke_test_after_wiring_everything() throws Exception {
-        WorkHours workHours = new WorkHours();
-        PersonalTimeOff personalTimeOff = new PersonalTimeOff();
-
         HashSet<WorkHoursPerDayAdjuster> adjusters = new HashSet<WorkHoursPerDayAdjuster>();
         adjusters.add(workHours);
         adjusters.add(personalTimeOff);
-        adjusters.add(new Holidays());
+        adjusters.add(holidays);
+        adjusters.add(conferenceDays);
+        adjusters.add(sickLeave);
 
         HoursRequired compendium = new WorkHoursPerDayCompendium(adjusters);
-
         LocalDate workday = DateTimeMother.AnyWorkday();
         DateTimeBuilder day = DateTimeBuilder.At(workday);
 
