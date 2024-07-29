e147f4c167e4016a516cc4c5f41283c0ba9ad9e7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-07-29 22:51:07 +0200

    Make proper class

--- plainjava/src/main/java/com/github/signed/timeless/Application.java
+++ plainjava/src/main/java/com/github/signed/timeless/Application.java
@@ -11,23 +11,30 @@ import java.io.PrintStream;
 
 class Application {
 
-    private final BalanceSheetConsoleUi consoleUi = new BalanceSheetConsoleUi();
-
     public static void main(String[] args) {
         new Application().run();
     }
 
+    private static class SampleYear extends WorkYear {
+
+        public SampleYear() {
+            super(2024);
+        }
+        @Override
+        public void july(DateTimeBuilder july) {
+            daysOffStarting(july.the(1).copy(), july.the(10).copy());
+            on(july.the(11)).workedFrom("17:30-22:16");
+            on(july.the(14)).workedFrom("11:11-12:10", "16:43-19:55");
+            on(july.the(15)).workedFrom("13:39-22:54");
+            on(july.the(16)).workedFrom("12:00-13:40");
+        }
+
+    }
+
+    private final BalanceSheetConsoleUi consoleUi = new BalanceSheetConsoleUi();
+
     private void run() {
-        WorkYear workYear = new WorkYear(2024) {
-            @Override
-            public void july(DateTimeBuilder july) {
-                daysOffStarting(july.the(1).copy(), july.the(10).copy());
-                on(july.the(11)).workedFrom("17:30-22:16");
-                on(july.the(14)).workedFrom("11:11-12:10", "16:43-19:55");
-                on(july.the(15)).workedFrom("13:39-22:54");
-                on(july.the(16)).workedFrom("12:00-13:40");
-            }
-        };
+        WorkYear workYear = new SampleYear();
 
         printBalance(workYear.balanceFor(new LocalDate(2024, 7, 1), new LocalDate()));
     }
