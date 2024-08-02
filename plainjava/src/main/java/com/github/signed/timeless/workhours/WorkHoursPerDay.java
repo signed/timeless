cc40ffaedc2b0f1b4644cc734e7ac51957c31522
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-08-02 21:54:25 +0200

    formatting

--- plainjava/src/main/java/com/github/signed/timeless/workhours/WorkHoursPerDay.java
+++ plainjava/src/main/java/com/github/signed/timeless/workhours/WorkHoursPerDay.java
@@ -5,18 +5,18 @@ import org.joda.time.Duration;
 
 public class WorkHoursPerDay {
 
-    private final Duration duration;
-
-    private WorkHoursPerDay(Duration duration) {
-        this.duration = duration;
-    }
-
     public static WorkHoursPerDay freeDay() {
         return unreducedWorkHours(Duration.ZERO);
     }
 
     public static WorkHoursPerDay unreducedWorkHours(Duration duration) {
         return new WorkHoursPerDay(duration);
+    }
+
+    private final Duration duration;
+
+    private WorkHoursPerDay(Duration duration) {
+        this.duration = duration;
     }
 
     public Duration duration() {
