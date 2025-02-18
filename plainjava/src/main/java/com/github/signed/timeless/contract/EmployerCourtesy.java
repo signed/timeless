355fc2b56a3b8115d553acbb33067521633cdf5b
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-02-18 23:28:04 +0100

    prepare for extract

--- plainjava/src/main/java/com/github/signed/timeless/contract/EmployerCourtesy.java
+++ plainjava/src/main/java/com/github/signed/timeless/contract/EmployerCourtesy.java
@@ -1,20 +1,38 @@
 package com.github.signed.timeless.contract;
 
+import com.github.signed.timeless.specialdays.SpecialDay;
+import com.github.signed.timeless.workhours.WorkHoursAdjuster;
 import com.github.signed.timeless.workhours.WorkHoursPerDayAdjuster;
 import com.github.signed.timeless.workhours.WorkHoursPerDayBuilder;
 import org.joda.time.LocalDate;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static com.github.signed.timeless.specialdays.SpecialDays.christmas;
 import static com.github.signed.timeless.specialdays.SpecialDays.newYearsEve;
 
 public class EmployerCourtesy implements WorkHoursPerDayAdjuster {
 
-    @Override
-    public void adjustHoursToWorkFor(LocalDate date, WorkHoursPerDayBuilder workHoursPerDayBuilder) {
+    public static List<SpecialDay> sampleSpecialDays() {
         var christmas = christmas();
         var newYearsEve = newYearsEve();
-        if (christmas.test(date) || newYearsEve.test(date)) {
-            workHoursPerDayBuilder.reduceByHalfAWorkDay();
+        return Arrays.asList(christmas, newYearsEve);
+    }
+
+    private final WorkHoursAdjuster adjuster;
+    private final List<SpecialDay> specialDays;
+
+    public EmployerCourtesy() {
+        adjuster = WorkHoursPerDayBuilder::reduceByHalfAWorkDay;
+        specialDays = sampleSpecialDays();
+    }
+
+    @Override
+    public void adjustHoursToWorkFor(LocalDate date, WorkHoursPerDayBuilder workHoursPerDayBuilder) {
+        final var flup = specialDays.stream().anyMatch(it -> it.test(date));
+        if (flup) {
+            adjuster.accept(workHoursPerDayBuilder);
         }
     }
 }
---
+++ plainjava/src/main/java/com/github/signed/timeless/workhours/WorkHoursAdjuster.java
@@ -0,0 +1,6 @@
+package com.github.signed.timeless.workhours;
+
+import java6.util.function.Consumer;
+
+public interface WorkHoursAdjuster extends Consumer<WorkHoursPerDayBuilder> {
+}
